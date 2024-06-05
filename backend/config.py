e89b4359546df9d82b4e6ca836e61ba25f091276
aliyoucef <66978921+aliyoucef599@users.noreply.github.com>
2024-06-05 22:18:43 +0100

    Add files via upload

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,7 @@
- 
 SECRET_KEY = "h%VIpb4|zl0'P[DHG93Ig3[(2j%C3l@>"
 DEBUG = True
-
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME="yamatolaurent@gmail.com"
+MAIL_PASSWORD= "inrc wuuu knsw svll"
